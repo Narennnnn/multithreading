0d80f7bb19c8455657e53459b840a4a5ad245654
Narendra Maurya <narendrrr@Narendras-MacBook-Air.local>
2024-08-03 01:39:06 +0530

    thread desc

--- CreateThreadThreadClass.java
+++ CreateThreadThreadClass.java
@@ -1,15 +1,18 @@
 class Thread1 extends Thread{
+    public Thread1(String threadName){
+        super(threadName);
+    }
     @Override
     public void run(){
         for(int i=0;i<5;i++){
-            System.out.println("Thread 1 running "+i);
+            System.out.println("Thread "+Thread.currentThread()+i);
         }
     }
 }
 public class CreateThreadThreadClass {
     public static void main(String[] args) {
         System.out.println("Main thread starting!");
-        Thread1 thread1=new Thread1();
+        Thread1 thread1=new Thread1("thread1");
         thread1.start();
         System.out.println("Main thread exiting!");
     }
