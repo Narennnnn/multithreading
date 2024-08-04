564fd3ec10d230f7c03f7263f5325c99eff952a5
Narendra Maurya <narendrrr@Narendras-MacBook-Air.local>
2024-08-04 23:11:51 +0530

    hashmap implementation with rehashing

--- Hashmap/MapTest.java
+++ Hashmap/MapTest.java
@@ -7,9 +7,13 @@ public class MapTest {
         mp.put(2, "prem");
         mp.put(3, "hey");
         mp.put(4, "hashmap");
+        mp.put(6, "xyz");
+        mp.put(7, "heyy");
+        mp.put(8, "lmnop");
         System.out.println(mp.get(4));
         System.out.println(mp.get(10));// should get null
         mp.remove(2);
         System.out.println(mp.get(2));
+
     }
 }
--- Hashmap/MyMap.java
+++ Hashmap/MyMap.java
@@ -1,76 +1,110 @@
 package Hashmap;
 import java.util.ArrayList;
 import java.util.List;
-public class MyMap <K,V>{
-    private List<MapNode<K,V>> bucket;// array of linked list where each index points to a linked list
-    private int capacity;// size of the bucket
-    private int size;//number of elements in hashmap
-    private final int INITIAL_CAPACITY=5;
-    public MyMap(){
+
+public class MyMap<K, V> {
+    private List<MapNode<K, V>> bucket; // array of linked list where each index points to a linked list
+    private int capacity; // size of the bucket
+    private int size; // number of elements in hashmap
+    private final int INITIAL_CAPACITY = 5;
+
+    public MyMap() {
         bucket = new ArrayList<>();
         capacity = INITIAL_CAPACITY;
-        for(int i=0;i<capacity;i++){
-            bucket.add(null);
+        for (int i = 0; i < capacity; i++) {
+            bucket.add(null); // initialize bucket with null values
         }
     }
-    private int getBucketIndex(K key){
-        int hashCode = key.hashCode();// hashCode() is a method of Object Class
+
+    private int getBucketIndex(K key) {
+        int hashCode = key.hashCode(); // hashCode() is a method of Object class
         return hashCode % capacity;
     }
-    public V get(K key){
+
+    public V get(K key) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K,V> head = bucket.get(bucketIndex);
-        while(head != null){
-            if(head.key.equals(key)){
+        MapNode<K, V> head = bucket.get(bucketIndex);
+        while (head != null) {
+            if (head.key.equals(key)) {
                 return head.value;
             }
-            head=head.next;
+            head = head.next;
         }
-        return null;//not found
+        return null; // not found
     }
-    public void put(K key, V value){
+
+    public void put(K key, V value) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K,V> head = bucket.get(bucketIndex);
-        while(head != null){
-            if(head.key.equals(key)){
-                head.value = value;// if key already exists then update the value
+        MapNode<K, V> head = bucket.get(bucketIndex);
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value; // if key already exists then update the value
                 return;
             }
-            head=head.next;
-        }     
+            head = head.next;
+        }
         size++;
-        MapNode<K,V> newEntry = new MapNode<K,V>(key,value);
+        MapNode<K, V> newEntry = new MapNode<>(key, value);
         head = bucket.get(bucketIndex);
-        newEntry.next = head;
-        bucket.set(bucketIndex,newEntry);
+        newEntry.next = head; // insert new node at the beginning of the linked list
+        bucket.set(bucketIndex, newEntry);
+        // to handle case if load factor size/capacity > 0.75 , we need to add another bucket to accomodate more key value pairs
+        double loadFactor = (1.0 * size)/capacity;
+        System.out.println("Inserting key "+key);
+        System.out.println("Load factor "+loadFactor);
+        if(loadFactor>0.7)
+         rehash();
     }
-    public void remove(K key){
+    public void rehash(){
+        System.out.println("Rehashing buckets ");
+        List<MapNode<K,V>> temp = bucket;
+        bucket = new ArrayList<>();
+        capacity = capacity*2;
+        for (int i = 0; i < capacity; i++) {
+            bucket.add(null); // initialize bucket with null values
+        }
+        size=0;
+        for(int i=0;i<temp.size();i++){
+            MapNode<K,V> head = temp.get(i);
+            while(head != null){
+                put(head.key,head.value);//rehash entire bucket
+                head = head.next;
+            }
+
+        }     
+    }
+
+    public void remove(K key) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K,V> head = bucket.get(bucketIndex);
-        MapNode<K,V> prev = null;
-        // find the node in the linked list which you want to delete: boils down to delete node from linked list
-        while(head != null){
-            if(head.key.equals(key)){
-                if(prev == null){
-                    bucket.set(bucketIndex, head.next);
-                }else{
-                    prev.next = head.next;
+        MapNode<K, V> head = bucket.get(bucketIndex);
+        MapNode<K, V> prev = null;
+        // find the node in the linked list which you want to delete: boils down to deleting node from linked list
+        while (head != null) {
+            if (head.key.equals(key)) {
+                if (prev == null) {
+                    bucket.set(bucketIndex, head.next); // remove head node
+                } else {
+                    prev.next = head.next; // bypass the current node
                 }
                 head.next = null;
-                size --;
+                size--;
                 break;
             }
             prev = head;
             head = head.next;
-        } 
+        }
     }
-    private class MapNode<K,V>{
+
+    private class MapNode<K, V> {
         K key;
         V value;
-        MapNode<K,V> next;
-        public MapNode(K key, V value){
-            this.key= key;
-            this.value=value;
+        MapNode<K, V> next;
+
+        public MapNode(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
     }
 }
+// refer : https://www.youtube.com/watch?v=sHpjxXX81Gw&t=1100s
+// special thanks to Riddhi Sir!
