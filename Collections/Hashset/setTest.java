ffd66e57e44b0da24e284b8f7fcd8bf1e79a1223
Narendra Maurya <narendrrr@Narendras-MacBook-Air.local>
2024-09-06 23:07:53 +0530

    feat: hashSet implementation

--- Collections/Hashset/setTest.java
+++ Collections/Hashset/setTest.java
@@ -41,6 +41,25 @@ public class setTest {
         System.out.println();
         for(String city: uniqueCity)
          System.out.print(city+" ");
+        // Hashset does not maintains order 
+        /*
+         * In order to maintain Order of elements: use LinkedHashSet class
+         * Set<Integer> nums = new LinkedHashSet<>();
+         */
+        System.out.println();
+        NavigableSet<Integer> treeSet = new TreeSet<>();// acts as Sorted Set by default stores in ascending order but passing lambda function can change the behaviour
+        for(int i=10;i>0;i--){
+            treeSet.add(i*10);
+        }
+        for(int val:treeSet)
+         System.out.print(val+", ");
+         System.out.println();
+        // floor(k) -> returns closest number just less than or equals to passed value 'k' 
+        System.out.println(treeSet.floor(15));
+        System.out.println();
+        // ceiling(k) -> returns closest number just greater than or equals to passed value 'k' 
+        System.out.println(treeSet.ceiling(22));
+        // Except whose performance is way better i.e O(1) remaining sets due to ordering takes logn time as Balance Binary tree implements them
     }
 }
 
